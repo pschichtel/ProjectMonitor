f611647cc6bc1ba2f90b3dcc04d6f18d6f9b96fd
Phillip Schichtel <phillip@schich.tel>
2022-06-05 01:46:50 +0200

    implement pagination
    also implement maximum concurrency for requests

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,13 @@ extern crate core;
 
 use std::cmp::Reverse;
 use std::error::Error;
+use std::fmt::Debug;
 use chrono::Utc;
+use futures::future::join_all;
 use graphql_client::{GraphQLQuery, Response};
 use serde::de::DeserializeOwned;
 use serde::{Serialize};
+use tokio::try_join;
 
 type URI = String;
 type DateTime = chrono::DateTime<Utc>;
@@ -16,7 +19,7 @@ type DateTime = chrono::DateTime<Utc>;
     query_path = "src/query.graphql",
     response_derives = "Debug",
 )]
-pub struct UserIssuesQuery;
+pub struct ViewerReposQuery;
 
 #[derive(GraphQLQuery)]
 #[graphql(
@@ -24,7 +27,23 @@ pub struct UserIssuesQuery;
     query_path = "src/query.graphql",
     response_derives = "Debug",
 )]
-pub struct OrgaIssuesQuery;
+pub struct ViewerOrganizationsQuery;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "src/github.graphql",
+    query_path = "src/query.graphql",
+    response_derives = "Debug",
+)]
+pub struct OrganizationReposQuery;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "src/github.graphql",
+    query_path = "src/query.graphql",
+    response_derives = "Debug",
+)]
+pub struct RepoIssuesQuery;
 
 #[derive(Debug, Clone)]
 pub struct Project {
@@ -42,20 +61,38 @@ pub struct Issue {
     author: String,
 }
 
-async fn query_issues<Req, Res>(
-    username: &str,
-    access_token: &str,
+struct ClientContext {
+    client: reqwest::Client,
+    username: String,
+    access_token: String,
+}
+
+impl PartialEq for repo_issues_query::SubscriptionState {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (&repo_issues_query::SubscriptionState::SUBSCRIBED, &repo_issues_query::SubscriptionState::SUBSCRIBED) => true,
+            (&repo_issues_query::SubscriptionState::UNSUBSCRIBED, &repo_issues_query::SubscriptionState::UNSUBSCRIBED) => true,
+            (&repo_issues_query::SubscriptionState::IGNORED, &repo_issues_query::SubscriptionState::IGNORED) => true,
+            _ => false,
+        }
+    }
+
+    fn ne(&self, other: &Self) -> bool {
+        !self.eq(other)
+    }
+}
+
+async fn run_query<Req, Res>(
+    context: &ClientContext,
     request_body: Req,
-    f: fn(&str, Res) -> Vec<Project>,
-) -> Result<Vec<Project>, Box<dyn Error>>
+) -> Result<Res, Box<dyn Error>>
     where
         Req: Serialize,
-        Res: DeserializeOwned {
-
-    let client = reqwest::Client::new();
-    let req = client.post("https://api.github.com/graphql")
+        Res: DeserializeOwned,
+        Res: Debug {
+    let req = context.client.post("https://api.github.com/graphql")
         .json(&request_body)
-        .basic_auth(username, Some(access_token))
+        .basic_auth(context.username.as_str(), Some(context.access_token.as_str()))
         .header("User-Agent", "ProjectMonitor");
     let response = req
         .send()
@@ -63,141 +100,178 @@ async fn query_issues<Req, Res>(
     let bytes = response.bytes().await?;
     let body: Response<Res> = serde_json::from_slice(&bytes)?;
 
-    Ok(f(username, body.data.unwrap()))
+    // println!("body={:?}", body);
+
+    Ok(body.data.unwrap())
 }
 
-impl PartialEq for user_issues_query::SubscriptionState {
-    fn eq(&self, other: &Self) -> bool {
-        match (self, other) {
-            (&user_issues_query::SubscriptionState::SUBSCRIBED, &user_issues_query::SubscriptionState::SUBSCRIBED) => true,
-            (&user_issues_query::SubscriptionState::UNSUBSCRIBED, &user_issues_query::SubscriptionState::UNSUBSCRIBED) => true,
-            (&user_issues_query::SubscriptionState::IGNORED, &user_issues_query::SubscriptionState::IGNORED) => true,
-            _ => false,
+#[derive(Debug)]
+struct Repo {
+    owner: String,
+    name: String,
+}
+
+async fn fetch_viewer_repos(context: &ClientContext) -> Result<Vec<Repo>, Box<dyn Error>> {
+    let mut output: Vec<Repo> = Vec::new();
+    let mut cursor: Option<String> = None;
+    loop {
+        let variables = viewer_repos_query::Variables { cursor: cursor.as_ref().map(|a| a.clone()) };
+        let result = run_query::<_, viewer_repos_query::ResponseData>(context, ViewerReposQuery::build_query(variables)).await?;
+        let values = result.viewer.repositories.edges.unwrap()
+            .into_iter()
+            .flatten()
+            .map(|edge| edge.node.unwrap())
+            .filter(|repo| !repo.is_archived)
+            .map(|repo| Repo { owner: repo.owner.login, name: repo.name });
+
+        output.extend(values);
+
+        let page_info = result.viewer.repositories.page_info;
+        cursor = page_info.end_cursor;
+        if !page_info.has_next_page {
+            break;
         }
     }
-
-    fn ne(&self, other: &Self) -> bool {
-        !self.eq(other)
-    }
+    Ok(output)
 }
 
-impl PartialEq for orga_issues_query::SubscriptionState {
-    fn eq(&self, other: &Self) -> bool {
-        match (self, other) {
-            (&orga_issues_query::SubscriptionState::SUBSCRIBED, &orga_issues_query::SubscriptionState::SUBSCRIBED) => true,
-            (&orga_issues_query::SubscriptionState::UNSUBSCRIBED, &orga_issues_query::SubscriptionState::UNSUBSCRIBED) => true,
-            (&orga_issues_query::SubscriptionState::IGNORED, &orga_issues_query::SubscriptionState::IGNORED) => true,
-            _ => false,
+async fn fetch_viewer_organizations(context: &ClientContext) -> Result<Vec<String>, Box<dyn Error>> {
+    let mut output: Vec<String> = Vec::new();
+    let mut cursor: Option<String> = None;
+    loop {
+        let variables = viewer_organizations_query::Variables { cursor: cursor.as_ref().map(|a| a.clone()) };
+        let result = run_query::<_, viewer_organizations_query::ResponseData>(context, ViewerOrganizationsQuery::build_query(variables)).await?;
+        let values = result.viewer.organizations.edges.unwrap()
+            .into_iter()
+            .flatten()
+            .map(|edge| edge.node.unwrap())
+            .filter(|orga| orga.viewer_can_administer)
+            .map(|orga| orga.login);
+
+        output.extend(values);
+
+        let page_info = result.viewer.organizations.page_info;
+        cursor = page_info.end_cursor;
+        if !page_info.has_next_page {
+            break;
         }
     }
+    Ok(output)
+}
 
-    fn ne(&self, other: &Self) -> bool {
-        !self.eq(other)
+async fn fetch_orga_repos(context: &ClientContext, login: &str) -> Result<Vec<Repo>, Box<dyn Error>> {
+    let mut output: Vec<Repo> = Vec::new();
+    let mut cursor: Option<String> = None;
+    loop {
+        let variables = organization_repos_query::Variables { login: login.to_string(), cursor: cursor.as_ref().map(|a| a.clone()) };
+        let result = run_query::<_, organization_repos_query::ResponseData>(context, OrganizationReposQuery::build_query(variables)).await?;
+        let orga_repos = result.organization.unwrap().repositories;
+        let values = orga_repos.edges.unwrap()
+            .into_iter()
+            .flatten()
+            .map(|edge| edge.node.unwrap())
+            .filter(|repo| !repo.is_archived)
+            .map(|repo| Repo { owner: repo.owner.login, name: repo.name });
+
+        output.extend(values);
+
+        let page_info = orga_repos.page_info;
+        cursor = page_info.end_cursor;
+        if !page_info.has_next_page {
+            break;
+        }
     }
+    Ok(output)
 }
 
-fn personal_projects(username: &str, data: user_issues_query::ResponseData) -> Vec<Project> {
-    data
-        .viewer
-        .repositories
-        .nodes
-        .unwrap()
-        .into_iter()
-        .flatten()
-        .filter(|repo| !repo.is_archived)
-        .map(|repo| {
-            let issues = repo.issues.nodes.unwrap().into_iter()
-                .flatten()
-                .filter(|issue| {
-                    issue.viewer_subscription.as_ref() != Some(&user_issues_query::SubscriptionState::SUBSCRIBED)
-                })
-                .map(|issue| Issue {
-                    title: issue.title,
-                    created_at: issue.created_at,
-                    url: issue.url,
-                    author: issue.author.unwrap().login,
-                })
-                .filter(|issue| issue.author != username)
-                .collect::<Vec<_>>();
-            Project {
-                url: repo.url,
-                name: repo.name,
-                owner: repo.owner.login,
-                issues,
-            }
-        })
-        .filter(|project| !project.issues.is_empty())
-        .collect::<Vec<_>>()
+async fn fetch_all_orga_repos(context: &ClientContext) -> Result<Vec<Repo>, Box<dyn Error>> {
+    let orgas = &fetch_viewer_organizations(&context).await?;
+    let mut futures = Vec::new();
+    for orga in orgas {
+        futures.push(fetch_orga_repos(context, orga.as_str()));
+    }
+
+    let results: Result<Vec<Vec<Repo>>, Box<dyn Error>> =
+        join_all(futures).await.into_iter().collect();
+
+    results.map(|nested| nested.into_iter().flatten().collect::<Vec<Repo>>())
 }
 
-fn organization_projects(username: &str, data: orga_issues_query::ResponseData) -> Vec<Project> {
-    data
-        .viewer
-        .organizations
-        .nodes
-        .unwrap()
-        .into_iter()
-        .flatten()
-        .flat_map(|orga| {
-            orga.repositories
-                .nodes
-                .unwrap()
-                .into_iter()
-                .flatten()
-                .filter(|repo| !repo.is_archived)
-                .map(|repo| {
-                    let issues = repo.issues.nodes.unwrap().into_iter()
-                        .flatten()
-                        .filter(|issue| {
-                            issue.viewer_subscription.as_ref() != Some(&orga_issues_query::SubscriptionState::SUBSCRIBED)
-                        })
-                        .map(|issue| Issue {
-                            title: issue.title,
-                            created_at: issue.created_at,
-                            url: issue.url,
-                            author: issue.author.unwrap().login,
-                        })
-                        .filter(|issue| issue.author != username)
-                        .collect::<Vec<_>>();
-                    Project {
-                        url: repo.url,
-                        name: repo.name,
-                        owner: repo.owner.login,
-                        issues,
-                    }
-                })
-                .filter(|project| !project.issues.is_empty())
-                .collect::<Vec<_>>()
-        })
-        .collect::<Vec<_>>()
+async fn fetch_all_repos(context: &ClientContext) -> Result<Vec<Repo>, Box<dyn Error>> {
+    let (viewer_repos, orga_repos) =
+        try_join!(fetch_viewer_repos(&context), fetch_all_orga_repos(&context))?;
+
+    let repos: Vec<Repo> = viewer_repos.into_iter()
+         .chain(orga_repos.into_iter())
+         .collect();
+
+    Ok(repos)
+}
+
+async fn fetch_project(context: &ClientContext, owner: &str, name: &str) -> Result<Project, Box<dyn Error>> {
+    let mut issues: Vec<Issue> = Vec::new();
+    let mut cursor: Option<String> = None;
+    let mut repo;
+    loop {
+        let variables = repo_issues_query::Variables { owner: owner.to_string(), name: name.to_string(), cursor: cursor.as_ref().map(|a| a.clone()) };
+        let result = run_query::<_, repo_issues_query::ResponseData>(context, RepoIssuesQuery::build_query(variables)).await?;
+        repo = result.repository.unwrap();
+        let values = repo.issues.edges.unwrap()
+            .into_iter()
+            .flatten()
+            .map(|edge| edge.node.unwrap())
+            .filter(|issue| issue.viewer_subscription.as_ref() != Some(&repo_issues_query::SubscriptionState::SUBSCRIBED))
+            .map(|issue| Issue { author: issue.author.map(|a| a.login).unwrap_or("<deleted user>".to_string()), url: issue.url, title: issue.title, created_at: issue.created_at })
+            .filter(|issue| issue.author != context.username);
+
+        issues.extend(values);
+
+        let page_info = repo.issues.page_info;
+        cursor = page_info.end_cursor;
+        if !page_info.has_next_page {
+            break;
+        }
+    }
+    issues.sort_by_key(|issue| Reverse(issue.created_at));
+    let project = Project {
+        url: repo.url,
+        name: name.to_string(),
+        owner: owner.to_string(),
+        issues,
+    };
+
+    Ok(project)
+}
+
+async fn fetch_all_projects(context: &ClientContext) -> Result<Vec<Project>, Box<dyn Error>> {
+    let repos = &fetch_all_repos(context).await?;
+    let mut futures = Vec::new();
+    for repo in repos {
+        futures.push(fetch_project(context, repo.owner.as_str(), repo.name.as_str()));
+    }
+
+    join_all(futures).await.into_iter().collect()
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let github_username = option_env!("GITHUB_USERNAME").unwrap();
     let github_access_token = option_env!("GITHUB_ACCESS_TOKEN").unwrap();
-    let personal = query_issues(
-        github_username,
-        github_access_token,
-        UserIssuesQuery::build_query(user_issues_query::Variables {}),
-        personal_projects,
-    ).await?;
-    let organization = query_issues(
-        github_username,
-        github_access_token,
-        OrgaIssuesQuery::build_query(orga_issues_query::Variables {}),
-        organization_projects,
-    ).await?;
-
-
-    let mut projects = personal.into_iter()
-        .chain(organization.into_iter())
-        .collect::<Vec<_>>();
+    let client = reqwest::Client::new();
+    let context = ClientContext {
+        client,
+        username: github_username.to_string(),
+        access_token: github_access_token.to_string()
+    };
+    let mut projects = fetch_all_projects(&context).await?;
     projects.sort_by_key(|project| {
         Reverse(project.clone().issues.into_iter().map(|i| i.created_at).max())
     });
 
     for project in &mut projects {
+        if project.issues.is_empty() {
+            continue;
+        }
         project.issues.sort_by_key(|issue| Reverse(issue.created_at));
 
         println!("Project: {}/{} ({})", project.owner, project.name, project.url);
