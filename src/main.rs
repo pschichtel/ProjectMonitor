07c03f7c4a7430b56a1f8110866365262e53ac6f
Phillip Schichtel <phillip@schich.tel>
2025-12-30 00:59:15 +0100

    factor out email code somewhat

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,11 @@ mod github;
 mod email;
 mod error;
 
+struct ResultingTasks {
+    new_known: Vec<Project>,
+    notify: Vec<Project>,
+}
+
 fn read_secret(name: &str) -> Option<String> {
     let direct_env_name = name.to_uppercase();
     match std::env::var(&direct_env_name) {
@@ -77,7 +82,7 @@ fn write_known_tasks(file: &mut File, projects: &Vec<Project>) -> Result<(), Box
     Ok(())
 }
 
-async fn check_and_notify_new_issues(github_context: &GithubClientContext, retain_for: Duration, email_context: &mut EmailContext, persistence_path: &str) -> Result<(), Box<dyn Error>> {
+async fn find_issues_for_notification(github_context: &GithubClientContext, retain_for: Duration, email_context: &mut EmailContext, persistence_path: &str) -> Result<(), Box<dyn Error>> {
     let mut file = File::options()
         .read(true)
         .write(true)
@@ -87,9 +92,11 @@ async fn check_and_notify_new_issues(github_context: &GithubClientContext, retai
     file.lock()?;
 
     let known_tasks = read_known_tasks(&mut file)?;
-    let new_tasks = check_and_notify_new_issues_(&github_context, retain_for, email_context, &known_tasks).await?;
-    if known_tasks != new_tasks {
-        write_known_tasks(&mut file, &new_tasks)?;
+    let ResultingTasks { new_known,  notify } = check_tasks_against_persistence(&github_context, retain_for, &known_tasks).await?;
+    // try notifying before writing the known tasks out, otherwise failed notifications will not be reattempted
+    notify_about_tasks(&notify, email_context)?;
+    if known_tasks != new_known {
+        write_known_tasks(&mut file, &new_known)?;
     }
 
     file.unlock().expect("failed to unlock persistence file!");
@@ -138,7 +145,7 @@ fn upsert_task<'a>(tasks: &'a mut Vec<Project>, project: &Project, task: &Task) 
     false
 }
 
-async fn check_and_notify_new_issues_(github_context: &GithubClientContext, retain_for: Duration, email_context: &mut EmailContext, known_tasks: &Vec<Project>) -> Result<Vec<Project>, Box<dyn Error>> {
+async fn check_tasks_against_persistence(github_context: &GithubClientContext, retain_for: Duration, known_tasks: &Vec<Project>) -> Result<ResultingTasks, Box<dyn Error>> {
     let now = chrono::Utc::now();
     let mut known_tasks = known_tasks.clone();
     let all_tasks = github::fetch_all_projects(&github_context).await?;
@@ -154,13 +161,17 @@ async fn check_and_notify_new_issues_(github_context: &GithubClientContext, reta
         }
     }
 
-    if !notify_tasks.is_empty() {
-        notify_tasks.sort_by_key(|project| {
-            Reverse(project.tasks.as_slice().into_iter().map(|i| i.created_at).max())
-        });
+    notify_tasks.sort_by_key(|project| {
+        Reverse(project.tasks.as_slice().into_iter().map(|i| i.created_at).max())
+    });
 
+    Ok(ResultingTasks { new_known: known_tasks, notify: notify_tasks })
+}
+
+fn notify_about_tasks(notify_tasks: &Vec<Project>, email_context: &mut EmailContext) -> Result<(), Box<dyn Error>> {
+    if !notify_tasks.is_empty() {
         let mut email_body = String::new();
-        for project in &notify_tasks {
+        for project in notify_tasks {
             email_body.push_str(format!("Project: {}/{} ({})\n", project.owner, project.name, project.url).as_str());
 
             for task in project.tasks.as_slice() {
@@ -181,10 +192,9 @@ async fn check_and_notify_new_issues_(github_context: &GithubClientContext, reta
             email_body.as_str(),
         )?;
     } else {
-        println!("No new unsubscribed issues!");
+        println!("No new tasks to notify about!");
     }
-
-    Ok(known_tasks)
+    Ok(())
 }
 
 fn get_env(name: &str) -> String {
@@ -282,7 +292,7 @@ async fn main() {
     });
 
     loop {
-        match check_and_notify_new_issues(&github_context, Duration::from_hours(24), &mut email_context, persistence_path.as_str()).await {
+        match find_issues_for_notification(&github_context, Duration::from_hours(24), &mut email_context, persistence_path.as_str()).await {
             Ok(_) => {
                 println!("Waiting for next check...")
             }
